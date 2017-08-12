eb5588a7b10ea2d2d060df8dcd310d848ba4ec6e
Matt Gibson <gothick@gothick.org.uk>
2017-08-12 23:19:44 +0100

    The most stubby of stubs.

---
+++ src/GoogleCloudPrint.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Gothick\GoogleCloudPrint;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+class GoogleCloudPrint {
+	function __construct() {
+	}
+	function print($document, $type, GoogleCloudPrinter $printer) {
+		
+	}
+}
---
+++ src/GoogleCloudPrinter.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gothick\GoogleCloudPrint;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+class GoogleCloudPrinter {
+
+}
