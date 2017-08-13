a96da529b8e185a3be8a8412a0ea002f37b61ad5
Matt Gibson <gothick@gothick.org.uk>
2017-08-13 16:17:33 +0100

    Bring in the Google API client and start using it. This project can now list availale printers, so that's a good start...

--- src/GoogleCloudPrint.php
+++ src/GoogleCloudPrint.php
@@ -4,10 +4,38 @@ namespace Gothick\GoogleCloudPrint;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+/**
+ * You *must* set GOOGLE_APPLICATION_CREDENTIALS to the path of a service account's key (JSON format)
+ * before constructing a GoogleCloudPrint.
+ * 
+ * @author matt
+ *
+ */
+
 class GoogleCloudPrint {
+	// TODO: When we move to PHP 7.1, we can use access modifiers on 
+	// class constants.
+	const APIBASE = 'https://www.google.com/cloudprint/';
+	
+	// TODO: Do we need to keep this hanging around? Or can we just set
+	// up the httpClient and use it without needing the Google_Client?
+	private $google_client;
+	private $httpClient;
+
 	function __construct() {
+		// Yes, Google have shoved their stuff in the global namespace. Sigh.
+		$this->client = new \Google_Client();
+		$this->client->useApplicationDefaultCredentials();
+		$this->client->addScope('https://www.googleapis.com/auth/cloudprint');
+		$this->httpClient = $this->client->authorize();
 	}
-	function print($document, $type, GoogleCloudPrinter $printer) {
-		
+
+	/*function print($document, $type, GoogleCloudPrinter $printer) {
+	}
+	*/
+	// search
+	function printers() {
+		$response = $this->httpClient->request('GET', self::APIBASE . 'search');
+		return (string) $response->getBody();
 	}
 }
--- src/GoogleCloudPrinter.php
+++ src/GoogleCloudPrinter.php
@@ -5,5 +5,18 @@ namespace Gothick\GoogleCloudPrint;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 class GoogleCloudPrinter {
+	private $id;
+	private $display_name;
 
+	// TODO: Maybe just throw JSON at this and have it rehydrate?
+	function __construct($id, $display_name) {
+		$this->id = $id;
+		$this->display_name = $display_name;
+	}
+	public function displayName() {
+		return $this->display_name;
+	}
+	public function id() {
+		return $this->id;
+	}
 }
